ca1818bd900ac27607f127d754a413cb6e6528f8
RondinDesBois <sam.germain@metropolia.fi>
2019-01-30 14:44:14 +0000

    lab1

---
+++ lcdip.c
@@ -0,0 +1,45 @@
+#include <wiringPi.h>          
+#include <lcd.h>                
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+//USE WIRINGPI PIN NUMBERS
+#define LCD_RS  25               //Register select pin
+#define LCD_E   24               //Enable Pin
+#define LCD_D4  23               //Data pin 4
+#define LCD_D5  22               //Data pin 5
+#define LCD_D6  21               //Data pin 6
+#define LCD_D7  14               //Data pin 7
+
+int main()
+{
+    int lcd;
+    wiringPiSetup();
+    lcd = lcdInit (2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0);
+
+    int n;
+    struct ifreq ifr;
+    char iface[] = "wlan0"; //Change this to the network of your choice (eth0, wlan0, etc.)
+
+    n = socket(AF_INET, SOCK_DGRAM, 0);
+    ifr.ifr_addr.sa_family = AF_INET;
+    strncpy(ifr.ifr_name , iface , IFNAMSIZ - 1);
+    ioctl(n, SIOCGIFADDR, &ifr);
+    close(n);
+
+    lcdPosition(lcd, 0, 0);
+    lcdPrintf(lcd, "IP Address: ");
+
+    lcdPosition(lcd, 0, 1);
+    lcdPrintf(lcd, ("%s - %s\n" , iface , inet_ntoa(( (struct sockaddr_in *)&ifr.ifr_addr )->sin_addr)));
+    return 0;
+}
+
